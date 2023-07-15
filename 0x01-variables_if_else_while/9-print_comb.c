b76c6b95eddde71a877fe96a2985a716d62659cc
Walter-Figo <maningowalter8@gmail.com>
2023-07-15 02:12:12 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prog Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('\n');
-		return (0);
 	}
+	putchar('\n');
+	return (0);
 }
