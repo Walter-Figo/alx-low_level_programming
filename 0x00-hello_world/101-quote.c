10974723ddef74c9dc7a3676096d7357fff18fa8
Walter-Figo <maningowalter8@gmail.com>
2023-07-17 02:31:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return: 1 (Successful)
+ * main - Entry
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", (59));
-return (1);
+	char buffer[1024] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, &buffer, strlen(buffer));
+	return (1);
 }
