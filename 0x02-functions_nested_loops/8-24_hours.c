005d1c028b88115e0d0217b146b2db9da80cf660
Walter-Figo <maningowalter8@gmail.com>
2023-07-18 09:30:44 -0700

    8-24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,12 +22,12 @@ void jack_bauer(void)
 						_putchar(a + '0');
 						_putchar(b + '0');
 						_putchar(58);
+						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
-
-}
-}
-}
-}
-}
+					}
+				}
+			}
+		}
+	}
 }
