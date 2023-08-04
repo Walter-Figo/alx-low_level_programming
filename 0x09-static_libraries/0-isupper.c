c688e8ba981da209b88f839a47f185a0bf01ea0f
Walter-Figo <maningowalter8@gmail.com>
2023-08-04 06:22:41 -0700

    my library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: The character being checked.
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
